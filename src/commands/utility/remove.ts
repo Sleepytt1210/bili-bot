2daab6f0e7bd981501a66041f890dc1a87f03a84
Dylon Wong <sleepytt1210@gmail.com>
2024-09-01 02:17:51 +0800

    Fix remove and resume

--- src/commands/utility/remove.ts
+++ src/commands/utility/remove.ts
@@ -1,40 +1,33 @@
 import {BaseCommand, CommandException} from "../base-command";
 import {CommandType} from "../command-type";
 import {GuildManager} from "../../app/guild";
-import {Message, EmbedBuilder} from "discord.js";
+import {EmbedBuilder, CacheType, ChatInputCommandInteraction, CommandInteractionOptionResolver, GuildMember} from "discord.js";
 import {helpTemplate, isNum} from "../../utils/utils";
 
 export class RemoveCommand extends BaseCommand {
 
-
     public constructor() {
-        super(['rm']);
+        super(['rm'], CommandType.REMOVE);
+        this.addIntegerOption((option) => option.setName("index").setDescription("Index of song in the queue.").setRequired(true))
     }
 
-    public name: CommandType = CommandType.REMOVE;
+    public async executeHandler(_: GuildMember, guild: GuildManager, args: Omit<CommandInteractionOptionResolver<CacheType>, "getMessage" | "getFocused">, interaction: ChatInputCommandInteraction): Promise<void> {
+        const index = args.getInteger("index");
+        if (index) {
+            const queue = guild.queueManager.queue;
 
-    public async executeHandler(member: GuildMember, guild: GuildManager, args: Omit<CommandInteractionOptionResolver<CacheType>, "getMessage" | "getFocused">, interaction: ChatInputCommandInteraction): Promise<void> {
-        const queue = guild.queueManager.queue;
-        if (args.length === 1 && isNum(args[0])) {
-            const index = Number(args.shift());
             if (!queue || queue.length === 0) {
                 throw CommandException.UserPresentable(`Queue is empty!`);
             }
             if (index < 1 || index > queue.length) throw CommandException.OutOfBound(queue.length);
-            await this.remove(guild, (index - 1));
+            this.remove(guild, (index - 1));
         } else {
-            throw CommandException.UserPresentable(`Invalid argument! Please enter a number!`);
+            throw CommandException.UserPresentable(`Index is required!`);
         }
     }
 
-    private async remove(guild: GuildManager, index: number): Promise<void> {
-        const name = await guild.queueManager.removeSong(index);
+    private remove(guild: GuildManager, index: number): void {
+        const name = guild.queueManager.removeSong(index);
         guild.printEvent(`${name.title} deleted from the queue`);
-    }
-
-    public helpMessage(guild: GuildManager): EmbedBuilder {
-        const res = helpTemplate(this);
-        res.addFields({name: 'Usage: ', value: `${guild.commandPrefix}${this.name} <index>`})
-        return res;
     }
 }
--- src/commands/utility/resume.ts
+++ src/commands/utility/resume.ts
@@ -1,28 +1,22 @@
 import {BaseCommand} from "../base-command";
 import {CommandType} from "../command-type";
 import {GuildManager} from "../../app/guild";
-import {Message, EmbedBuilder} from "discord.js";
+import {EmbedBuilder, CacheType, ChatInputCommandInteraction, CommandInteractionOptionResolver, GuildMember} from "discord.js";
 import {helpTemplate} from "../../utils/utils";
 
 export class ResumeCommand extends BaseCommand {
 
 
     public constructor() {
-        super(['r', 'unpause', 'continue']);
+        super(['r', 'unpause', 'continue'], CommandType.RESUME);
     }
 
     public name: CommandType = CommandType.RESUME;
 
-    public async executeHandler(member: GuildMember, guild: GuildManager, args: Omit<CommandInteractionOptionResolver<CacheType>, "getMessage" | "getFocused">, interaction: ChatInputCommandInteraction): Promise<void> {
+    public async executeHandler(member: GuildMember, guild: GuildManager, _: Omit<CommandInteractionOptionResolver<CacheType>, "getMessage" | "getFocused">, interaction: ChatInputCommandInteraction): Promise<void> {
         guild.checkMemberInChannel(member);
         if (guild.queueManager.resume()) {
             guild.printEvent(`Audio resumed`);
         }
-    }
-
-    public helpMessage(guild: GuildManager): EmbedBuilder {
-        const res = helpTemplate(this);
-        res.addFields({name: 'Usage: ', value: `${guild.commandPrefix}${this.name}`});
-        return res;
     }
 }
